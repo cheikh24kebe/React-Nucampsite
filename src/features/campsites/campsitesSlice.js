88c0b9acbeda69b361045e09f4010ebc511018dc
Cheikh Kebe <pro24kebe@icloud.com>
2025-06-08 23:20:27 -0400

    useState

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -4,7 +4,6 @@ export const selectAllCampsites = () => {
     return CAMPSITES;
 };
 
-export const selectRandomCampsite = () => {
-    const randomIndex = Math.floor(Math.random() * CAMPSITES.length);
-    return CAMPSITES[randomIndex];
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find((campsite) => campsite.id === id);
 };
