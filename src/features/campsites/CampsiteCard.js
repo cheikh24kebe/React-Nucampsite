a6ad7491a09c36354a7176931e5fe4ebbbd68944
Cheikh Kebe <pro24kebe@icloud.com>
2025-06-09 18:18:23 -0400

    CampsiteDetailPage with useParams

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,17 +1,19 @@
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const CampsiteCard = (props) => {
+    const { id, image, name } = campsite;
+
     return (
-        <Card>
-            <CardImg
-                width='100%'
-                src={props.campsite.image}
-                alt={props.campsite.name}
-            />
-            <CardImgOverlay>
-                <CardTitle>{props.campsite.name}</CardTitle>
-            </CardImgOverlay>
-        </Card>
+        <Link to={`${id}`}>
+            <Card>
+                <CardImg src={image} alt={name} />
+                <CardImgOverlay>
+                    <CardTitle>{name}</CardTitle>
+                </CardImgOverlay>
+            </Card>
+        </Link>
+
     );
 };
 
