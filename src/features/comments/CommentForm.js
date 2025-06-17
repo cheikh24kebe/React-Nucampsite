1042d92061fab0aa38b1d8ef8f3d4ec437d89aef
Cheikh Kebe <pro24kebe@icloud.com>
2025-06-16 22:41:01 -0400

    ContactPage

---
+++ src/features/comments/CommentForm.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, FormGroup, Label } from 'reactstrap';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { validateCommentForm } from '../../utils/validateCommentForm';
+
+export default function CommentForm({ campsiteId }) {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleSubmit = values => {
+    const comment = {
+      id: values.id,
+      campsiteId: parseInt(campsiteId),
+      rating: parseInt(values.rating),
+      text: values.commentText,
+      author: values.author
+    };
+    console.log(comment);
+    setModalOpen(false);
+  };
+
+  return (
+    <>
+      <Button outline onClick={() => setModalOpen(true)}>
+        <i className="fa fa-pencil fa-lg" /> Add Comment
+      </Button>
+      <Modal isOpen={modalOpen}>
+        <ModalHeader toggle={() => setModalOpen(false)}>
+          Add Comment
+        </ModalHeader>
+        <ModalBody>
+          <Formik
+            initialValues={{ rating: undefined, author: '', commentText: '' }}
+            validate={validateCommentForm}
+            onSubmit={handleSubmit}
+          >
+            <Form>
+              <FormGroup>
+                <Label htmlFor="rating">Rating</Label>
+                <Field name="rating" as="select" className="form-control">
+                  <option>Select...</option>
+                  {[1, 2, 3, 4, 5].map(n => <option key={n}>{n}</option>)}
+                </Field>
+                <ErrorMessage name="rating">
+                  {msg => <div className="text-danger">{msg}</div>}
+                </ErrorMessage>
+              </FormGroup>
+
+              <FormGroup>
+                <Label htmlFor="author">Your Name</Label>
+                <Field name="author" placeholder="Your Name" className="form-control" />
+                <ErrorMessage name="author">
+                  {msg => <div className="text-danger">{msg}</div>}
+                </ErrorMessage>
+              </FormGroup>
+
+              <FormGroup>
+                <Label htmlFor="commentText">Comment</Label>
+                <Field name="commentText" as="textarea" rows="12" className="form-control" />
+              </FormGroup>
+
+              <Button type="submit" color="primary">Submit</Button>
+            </Form>
+          </Formik>
+        </ModalBody>
+      </Modal>
+    </>
+  );
+}
--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,25 +1,25 @@
+import React from 'react';
 import { Col } from 'reactstrap';
 import Comment from './Comment';
+import CommentForm from './CommentForm';
 import { selectCommentsByCampsiteId } from './commentsSlice';
 
 const CommentsList = ({ campsiteId }) => {
-    const comments = selectCommentsByCampsiteId(campsiteId);
+  const comments = selectCommentsByCampsiteId(campsiteId);
 
-    if (comments && comments.length > 0) {
-        return (
-            <Col md='5' className='m-1'>
-                <h4>Comments</h4>
-                {comments.map((comment) => {
-                    return <Comment key={comment.id} comment={comment} />;
-                })}
-            </Col>
-        );
-    }
-    return (
-        <Col md='5' className='m-1'>
-            There are no comments for this campsite yet.
-        </Col>
-    );
+  return (
+    <Col md="5" className="m-1">
+      <h4>Comments</h4>
+      {comments && comments.length > 0 ? (
+        comments.map(comment => (
+          <Comment key={comment.id} comment={comment} />
+        ))
+      ) : (
+        <p>There are no comments for this campsite yet.</p>
+      )}
+      <CommentForm campsiteId={campsiteId} />
+    </Col>
+  );
 };
 
 export default CommentsList;
