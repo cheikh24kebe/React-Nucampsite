689dc34ba005bfaa94f9b66f40636728f948af0b
Cheikh Kebe <pro24kebe@icloud.com>
2025-06-26 01:05:34 -0400

    addComment

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -1,68 +1,117 @@
-import React, { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, FormGroup, Label } from 'reactstrap';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addComment } from './commentsSlice';
+import {
+    Button,
+    Label,
+    Modal,
+    ModalBody,
+    ModalHeader
+} from 'reactstrap';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
-import { validateCommentForm } from '../../utils/validateCommentForm';
 
-export default function CommentForm({ campsiteId }) {
-  const [modalOpen, setModalOpen] = useState(false);
+const CommentForm = ({ campsiteId }) => {
+    const [modalOpen, setModalOpen] = useState(false);
+    const dispatch = useDispatch();
 
-  const handleSubmit = values => {
-    const comment = {
-      id: values.id,
-      campsiteId: parseInt(campsiteId),
-      rating: parseInt(values.rating),
-      text: values.commentText,
-      author: values.author
+    const handleSubmit = (values) => {
+        const comment = {
+            id: values.id,
+            campsiteId: parseInt(campsiteId),
+            rating: parseInt(values.rating),
+            text: values.commentText,
+            author: values.author,
+            date: new Date(Date.now()).toISOString()
+        };
+
+        console.log('Submitted comment:', comment);
+        dispatch(addComment(comment));
+        setModalOpen(false);
     };
-    console.log(comment);
-    setModalOpen(false);
-  };
 
-  return (
-    <>
-      <Button outline onClick={() => setModalOpen(true)}>
-        <i className="fa fa-pencil fa-lg" /> Add Comment
-      </Button>
-      <Modal isOpen={modalOpen}>
-        <ModalHeader toggle={() => setModalOpen(false)}>
-          Add Comment
-        </ModalHeader>
-        <ModalBody>
-          <Formik
-            initialValues={{ rating: undefined, author: '', commentText: '' }}
-            validate={validateCommentForm}
-            onSubmit={handleSubmit}
-          >
-            <Form>
-              <FormGroup>
-                <Label htmlFor="rating">Rating</Label>
-                <Field name="rating" as="select" className="form-control">
-                  <option>Select...</option>
-                  {[1, 2, 3, 4, 5].map(n => <option key={n}>{n}</option>)}
-                </Field>
-                <ErrorMessage name="rating">
-                  {msg => <div className="text-danger">{msg}</div>}
-                </ErrorMessage>
-              </FormGroup>
+    return (
+        <>
+            <Button outline onClick={() => setModalOpen(true)}>
+                <i className='fa fa-pencil fa-lg' /> Submit Comment
+            </Button>
 
-              <FormGroup>
-                <Label htmlFor="author">Your Name</Label>
-                <Field name="author" placeholder="Your Name" className="form-control" />
-                <ErrorMessage name="author">
-                  {msg => <div className="text-danger">{msg}</div>}
-                </ErrorMessage>
-              </FormGroup>
+            <Modal isOpen={modalOpen} toggle={() => setModalOpen(!modalOpen)}>
+                <ModalHeader toggle={() => setModalOpen(!modalOpen)}>
+                    Submit Comment
+                </ModalHeader>
 
-              <FormGroup>
-                <Label htmlFor="commentText">Comment</Label>
-                <Field name="commentText" as="textarea" rows="12" className="form-control" />
-              </FormGroup>
+                <ModalBody>
+                    <Formik
+                        initialValues={{
+                            rating: '',
+                            author: '',
+                            commentText: ''
+                        }}
+                        onSubmit={handleSubmit}
+                        validate={validateCommentForm}
+                    >
+                        <Form>
+                            <Label htmlFor='rating'>Rating</Label>
+                            <Field name='rating' as='select' className='form-control'>
+                                <option value=''>Select...</option>
+                                <option value='1'>1</option>
+                                <option value='2'>2</option>
+                                <option value='3'>3</option>
+                                <option value='4'>4</option>
+                                <option value='5'>5</option>
+                            </Field>
+                            <ErrorMessage name='rating'>
+                                {(msg) => <p className='text-danger'>{msg}</p>}
+                            </ErrorMessage>
 
-              <Button type="submit" color="primary">Submit</Button>
-            </Form>
-          </Formik>
-        </ModalBody>
-      </Modal>
-    </>
-  );
-}
+                            <Label htmlFor='author'>Your Name</Label>
+                            <Field
+                                name='author'
+                                placeholder='Your Name'
+                                className='form-control'
+                            />
+                            <ErrorMessage name='author'>
+                                {(msg) => <p className='text-danger'>{msg}</p>}
+                            </ErrorMessage>
+
+                            <Label htmlFor='commentText'>Comment</Label>
+                            <Field
+                                name='commentText'
+                                as='textarea'
+                                rows='6'
+                                className='form-control'
+                            />
+                            <ErrorMessage name='commentText'>
+                                {(msg) => <p className='text-danger'>{msg}</p>}
+                            </ErrorMessage>
+
+                            <Button type='submit' color='primary'>
+                                Submit
+                            </Button>
+                        </Form>
+                    </Formik>
+                </ModalBody>
+            </Modal>
+        </>
+    );
+};
+
+const validateCommentForm = (values) => {
+    const errors = {};
+    if (!values.rating) {
+        errors.rating = 'Required';
+    }
+    if (!values.author) {
+        errors.author = 'Required';
+    } else if (values.author.length < 2) {
+        errors.author = 'Must be at least 2 characters.';
+    } else if (values.author.length > 15) {
+        errors.author = 'Must be 15 characters or less.';
+    }
+    if (!values.commentText) {
+        errors.commentText = 'Required';
+    }
+    return errors;
+};
+
+export default CommentForm;
--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,25 +1,31 @@
-import React from 'react';
 import { Col } from 'reactstrap';
 import Comment from './Comment';
 import CommentForm from './CommentForm';
+import { useSelector } from 'react-redux';
 import { selectCommentsByCampsiteId } from './commentsSlice';
 
 const CommentsList = ({ campsiteId }) => {
-  const comments = selectCommentsByCampsiteId(campsiteId);
+    const comments = useSelector(selectCommentsByCampsiteId(campsiteId));
 
-  return (
-    <Col md="5" className="m-1">
-      <h4>Comments</h4>
-      {comments && comments.length > 0 ? (
-        comments.map(comment => (
-          <Comment key={comment.id} comment={comment} />
-        ))
-      ) : (
-        <p>There are no comments for this campsite yet.</p>
-      )}
-      <CommentForm campsiteId={campsiteId} />
-    </Col>
-  );
+    if (comments && comments.length > 0) {
+        return (
+            <Col md='5' className='m-1'>
+                <h4>Comments</h4>
+                {comments.map((comment) => (
+                    <Comment key={comment.id} comment={comment} />
+                ))}
+                <CommentForm campsiteId={campsiteId} />
+            </Col>
+        );
+    }
+
+    return (
+        <Col md='5' className='m-1'>
+            <h4>Comments</h4>
+            <p>There are no comments for this campsite yet.</p>
+            <CommentForm campsiteId={campsiteId} />
+        </Col>
+    );
 };
 
 export default CommentsList;
